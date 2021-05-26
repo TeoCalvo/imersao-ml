8d25b7c38eaf9dd5d47b27f1e5b1d8229496cb42
Téo Calvo <teocalvo2@gmail.com>
2021-05-25 21:31:50 -0300

    Meio da 1a aula

--- src/regressao.py
+++ src/regressao.py
@@ -1,7 +1,7 @@
-
 # %%
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
 np.random.seed(28)
@@ -13,3 +13,44 @@ data['Nota'] = data['Nota'].abs()
 data["Nota"] = data["Nota"].apply(lambda x: x if x < 10 else 10)
 
 # Fazer os modelos na aula!!!!
+
+plt.plot( data["Cerveja"], data["Nota"], "o" )
+plt.grid(True)
+plt.xlabel("Cerveja")
+plt.ylabel("Nota")
+plt.title("Relação Cerveja e Nota")
+
+# %%
+
+# CRIANDO UM MODELO DE REGRESSÃO LINEAR
+
+from sklearn import linear_model
+
+# Esse é o nome modelo de regressão linear
+model = linear_model.LinearRegression(fit_intercept=True)
+
+# %%
+
+# Vamos ajustar o modelo
+
+X = data[ ["Cerveja"] ] # Isso retorn um DF
+y = data["Nota"] # Isso retorna uma Series
+model.fit(X, y) # Ajuste dos parâmetros beta0 e beta1
+
+# %%
+b0, b1 = model.intercept_, model.coef_[0]
+
+print(f" Beta0 = {b0} | Beta1 = {b1}")
+
+# %%
+
+values = [[i] for i in range(0,11)] # Novo X
+y_pred = model.predict(values) # Predição de y ou y chapeu
+
+plt.plot( data["Cerveja"], data["Nota"], "o" ) # y verdadeiro
+plt.plot( values, y_pred, "-" ) # y estimado
+plt.grid(True)
+plt.xlabel("Cerveja")
+plt.ylabel("Nota")
+plt.title("Relação Cerveja e Nota")
+plt.legend(["Observações", "Modelo"])
